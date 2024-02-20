a6bdd15f280b5f8f5cacee4106bde91eca79f361
阿龙 <425633796@qq.com>
2024-02-20 16:18:46 +0800

    feat(flink): 1.flink task initializing

--- my-admin/src/main/java/com/admin/service/impl/StreamServiceImpl.java
+++ my-admin/src/main/java/com/admin/service/impl/StreamServiceImpl.java
@@ -3,11 +3,18 @@ package com.admin.service.impl;
 import com.admin.service.StreamService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.assertj.core.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.IntStream;
 
 /**
  * @Description TODO
@@ -19,21 +26,63 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class StreamServiceImpl implements StreamService {
 
+    public static final String MASTER = "MASTER";
+
+    public static final String SLAVE = "SLAVE";
+
     @Override
     public void calculate() {
         StreamExecutionEnvironment execution = StreamExecutionEnvironment.getExecutionEnvironment();
         execution.setRuntimeMode(RuntimeExecutionMode.STREAMING);
-        execution.addSource(new SourceFunction<Object>() {
-            @Override
-            public void run(SourceContext<Object> sourceContext) throws Exception {
 
+        //source
+        DataStreamSource<String> source = execution.addSource(new SourceFunction<String>() {
+
+            @Override
+            public void run(SourceContext<String> sourceContext) throws Exception {
+                sourceContext.emitWatermark(new Watermark(60 * 1000));
+                IntStream.range(1, 100)
+                         .boxed()
+                         .map(String::valueOf)
+                         .forEach(sourceContext::collect);
             }
 
             @Override
             public void cancel() {
 
             }
+
         });
+
+        //operator
+        SingleOutputStreamOperator<Pair<String, String>> operator = source.filter(one -> !Strings.isNullOrEmpty(one))
+                                                                          .map(s -> {
+                                                                              if (Integer.parseInt(s) % 10 == 0) {
+                                                                                  return Pair.of(MASTER, s);
+                                                                              }
+                                                                              return Pair.of(SLAVE, s);
+                                                                          })
+                                                                          .keyBy(Pair::getLeft)
+                                                                          .sum(1);
+
+        //sink
+        operator.print();
+
+        //customer define sink
+        //operator.addSink(new SinkFunction<Pair<String, String>>() {
+        //    @Override
+        //    public void invoke(Pair<String, String> value) throws Exception {
+        //        SinkFunction.super.invoke(value);
+        //    }
+        //});
+
+        //execute
+        try {
+            execution.execute();
+        } catch (Exception e) {
+            log.error("Flink task execute error:", e);
+            throw new RuntimeException(e);
+        }
     }
 
 }
